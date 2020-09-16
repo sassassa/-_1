326936500ee166eb1f15825131a966dc1fa3997c
MinsooKim <alstn9008@naver.com>
2020-09-16 15:02:56 +0900

    add MyApp

---
+++ main/MyApp.java
@@ -0,0 +1,26 @@
+package main;
+
+/**
+ * Write a description of class MyApp here.
+ *
+ * @author (2017210105 KimMinsoo)
+ * @version (2020.09.16)
+ */
+public class MyApp
+{
+    public static void main(String[] args)
+    {
+        Integer[] array;
+        String data = "2,3,5,2,3,10";
+        array = makeToken(data);
+        Circle c1 = makeCircle(array[0], array[1], array[2]);
+        Circle c2 = makeCircle(array[3], array[4], array[5]);
+        
+        System.out.println("원1 : " +c1 + " ==> 면적 : " + c1.getArea());
+        System.out.println("원2 : " +c2 + " ==> 면적 : " + c2.getArea());
+        if(c1 == c2)
+            System.out.println("같은 원");
+        else
+            System.out.println("서로 다른 원");
+    }
+}
